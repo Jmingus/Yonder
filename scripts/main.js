fcc6365e1e46e8be9d04c5505c8e2fdce6cc3e0d
Jmingus <jmingus@austin.rr.com>
2015-09-24 16:33:35 -0500

    put in routing boilerplate

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,25 @@
 'use strict';
 var Backbone = require('backbone');
 var $ = require('jquery');
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'landingPage',
+    'maps': 'mapsPage',
+    'following': 'followingPage'
+  },
+  landingPage: function(){
+
+  },
+  mapsPage: function(){
+
+  },
+  followingPage: function(){
+
+  }
+});
+
+var app = new Router();
+Backbone.history.start();
 
 $(document).ready(function(){
 
