651972f41af4f95326094f36021d900e57419aed
James <james.stitts@gmail.com>
2015-09-24 19:22:43 -0500

    added html input boxes for adding new events

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -12664,7 +12664,7 @@ return jQuery;
 
 var Backbone = require('backbone');
 var EventModel = require('../models/EventModel');
-module.exports = Backbone.collection.extend({
+module.exports = Backbone.Collection.extend({
 	model: EventModel,
 	url: 'http://tiyfe.herokuapp.com/collections/EventLogging'
 });
