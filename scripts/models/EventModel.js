a23c67183786886030d8bac828ddc9fdf7ea5f1b
Jmingus <jmingus@austin.rr.com>
2015-09-27 15:43:27 -0500

    working on formSubmit and posting

--- scripts/models/EventModel.js
+++ scripts/models/EventModel.js
@@ -1,37 +1,38 @@
 var Backbone = require('backbone');
 module.exports = Backbone.Model.extend({
 	defaults:{
-		id: null,
-		official: true,
+		official: false,
 		name: '',
 		day_id: null,
-		time_of_day_id: null,
-		venue: null,
+		time_of_day_id: '',
+		venue: '',
 		category_id: null,
 		location_id: null,
+        description: '',
+        specific_time: ''
 	},
 	urlRoot: 'https://yonder.herokuapp.com/events',
 	parse: function(data){
 		if(parseInt(data.day_id) === 1){
-            data.day_id = 'Monday'
+            data.day_id = 'Monday';
 			return data;
 		}else if(parseInt(data.day_id) === 2){
-            data.day_id = 'Tuesday'
+            data.day_id = 'Tuesday';
 			return data;
 		}else if(parseInt(data.day_id) === 3){
-            data.day_id = 'Wednesday'
+            data.day_id = 'Wednesday';
 			return data;
 		}else if(parseInt(data.day_id) === 4){
-            data.day_id = 'Thursday'
+            data.day_id = 'Thursday';
 			return data;
 		}else if(parseInt(data.day_id) === 5){
-            data.day_id = 'Friday'
+            data.day_id = 'Friday';
 			return data;
 		}else if(parseInt(data.day_id) === 6){
-            data.day_id = 'Saturday'
+            data.day_id = 'Saturday';
 			return data;
 		}else if(parseInt(data.day_id) === 7){
-            data.day_id = 'Sunday'
+            data.day_id = 'Sunday';
 			return data;
 		}else{
             return data
